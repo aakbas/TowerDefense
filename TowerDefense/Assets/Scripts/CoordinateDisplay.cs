5a5cb4c6def6dfefa47ec5efaa598d569b567941
Abdullah Akbaş <akbas.abdullah77@gmail.com>
2021-08-14 20:08:34 +0300

    Coordinates Adjusment

--- TowerDefense/Assets/Scripts/CoordinateDisplay.cs
+++ TowerDefense/Assets/Scripts/CoordinateDisplay.cs
@@ -11,27 +11,48 @@ public class CoordinateDisplay : MonoBehaviour
     private TextMeshPro coordinateLabel;
     private int coordinateX;
     private int coordinateY;
+    private bool isGameStarted = false;
 
     void Awake()
     {
         coordinateLabel = GetComponent<TextMeshPro>();
+        
     }
 
     void Update()
     {
         if (!Application.isPlaying)
         {
-            DisplayCordinates();            
+            DisplayCordinates();
+            UpdateObjectName();
+        }
+        else
+        {
+            if (!isGameStarted)
+            {
+                coordinateLabel.enabled = false;
+                isGameStarted = true;
+            }
         }
     }
 
     private void DisplayCordinates()
     {
-        coordinateX = Mathf.RoundToInt(transform.parent.position.x);
-        coordinateY = Mathf.RoundToInt(transform.parent.position.z);
+        CalculateCoordinates();
         coordinateLabel.text = $"({ coordinateX},{coordinateY})";
-        
 
+    }
+
+    private void CalculateCoordinates()
+    {
+        coordinateX = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
+        coordinateY = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.y);
+    }
+
+    private void UpdateObjectName()
+    {
+        CalculateCoordinates();
+        transform.parent.name = $"({ coordinateX},{coordinateY})";
     }
 
 }
